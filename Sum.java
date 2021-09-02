0e5188bbe4c6bc7c4cc097b4344a56959a7f21f5
Pradeep <74000008+Pradeepreddy580@users.noreply.github.com>
2021-09-02 12:09:12 +0530

    Create Sum.java

---
+++ Sum.java
@@ -0,0 +1,8 @@
+public class Sum{
+  public static void main(String[]args){
+    int a = 400;
+    int b = 500;
+    int sum = a+b;
+    System.out.println("The sum of two numbers "+a+" and "+b+" is "+sum);
+  }
+}
